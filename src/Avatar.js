7090631650350fa95d551b8fa73b743aee54a9d8
Dante Razo <dante@razonet.org>
2018-07-02 23:05:48 -0400

    Add Avatar component

---
+++ src/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Author from './Author'
+
+const Avatar = () =>{
+	return (
+		<div className="avatar">
+			<img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author"/>
+			<Author/>
+		</div>
+	)
+}
+
+export default Avatar
