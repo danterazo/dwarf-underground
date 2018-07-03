9d77e5572a5bc2d366c07360d33120bd8e24060b
Dante Razo <dante@razonet.org>
2018-07-02 23:05:12 -0400

    Add ArticleTitle component

---
+++ src/ArticleTitle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Avatar from './Avatar'
+
+const ArticleTitle = () =>{
+	return (
+		<span>
+			<h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+			<Avatar/>
+		</span>
+	)
+}
+
+export default ArticleTitle
